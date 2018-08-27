e9382992a6ec8247e2b0eb63ef934b9db932abc8
yamaimo <yarnaimo@gmail.com>
2018-08-27 23:40:34 +0900

    Implement Twitter class

---
+++ src/__test__/twitter.test.ts
@@ -0,0 +1,33 @@
+import config from 'config'
+import { Twitter } from '..'
+import { ITweet } from '../Tweet'
+
+const twitterConfig = config.get<any>('twitter')
+const twitter = new Twitter(twitterConfig)
+
+describe('Twitter', () => {
+    test('get 3 tweets from home timeline', async () => {
+        const tweets = await twitter.get<ITweet[]>('statuses/home_timeline', {
+            count: 3,
+            a: null,
+        })
+        expect(tweets.length).toBe(3)
+    })
+
+    test('post DM to myself and delete it', async () => {
+        const text = 'soko pick up ' + new Date().toISOString()
+
+        const posted = await twitter.post<any>('direct_messages/new', {
+            user_id: twitterConfig.userId,
+            text,
+            hoge: undefined,
+        })
+        expect(posted.text).toBe(text)
+        expect(posted.recipient.id_str).toBe(twitterConfig.userId)
+
+        const deleted = await twitter.post<any>('direct_messages/destroy', {
+            id: posted.id_str,
+        })
+        expect(deleted.id_str).toBe(posted.id_str)
+    })
+})
